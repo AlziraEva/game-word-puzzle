3027d55cf120be86cc02d329db2b471b184dbccc
Eva <alziralves1996@gmail.com>
2024-04-28 19:10:53 -0300

    implementação do botão reset

---
+++ js/animals.js
@@ -0,0 +1,24 @@
+// Informações sobre os animais do jogo
+var games = [ 
+    { img: 'img/koala.png', color:'#176580', word: 'koala', sound: '' }, 
+    { img: 'img/elephant1.png', color:'#a36513', word: 'elephant', sound: 'sounds/elephant' },
+    { img: 'img/monkey.png', color:'#ffc48b', word: 'monkey', sound: 'sounds/monkey' },
+    { img: 'img/bear.png', color:'#807148', word: 'bear', sound: 'sounds/bear' },
+    { img: 'img/horse.png', color:'#bc9e6c', word: 'horse', sound: 'sounds/horse' },
+    { img: 'img/bull.png', color:'#ff5f09', word: 'bull', sound: 'sounds/bull' },
+    { img: 'img/rabbit.png', color:'#c81f27', word: 'rabbit', sound: '' },
+    { img: 'img/tiger.png', color:'#b3eef4', word: 'tiger', sound: 'sounds/meow' },
+    { img: 'img/turtle.png', color:'#d5ea86', word: 'turtle', sound: '' },
+    { img: 'img/lion1.png', color:'#dd992d', word: 'lion', sound: 'sounds/lion' },
+    { img: 'img/cicada.png', color:'#008000', word: 'cicada', sound: 'sounds/cicada' },
+    { img: 'img/fox.png', color:'#ffa500', word: 'fox', sound: 'sounds/fox' },
+    { img: 'img/pork.png', color:'#ffb6c1', word: 'pork', sound: 'sounds/pork' },
+    { img: 'img/whale.png', color:'#add8e6', word: 'whale', sound: 'sounds/whale' },
+    { img: 'img/snake.png', color:'#006400', word: 'snake', sound: 'sounds/snake' },
+    { img: 'img/fish.png', color:'#3232CD', word: 'fish', sound: 'sounds/fish' },
+    { img: 'img/frog.png', color:'#90ee90', word: 'frog', sound: 'sounds/frog' },
+    { img: 'img/dolphin.png', color:'#00ffff', word: 'dolphin', sound: 'sounds/dolphin' },
+    { img: 'img/shark.png', color:'#87cefa', word: 'shark', sound: 'sounds/shark' },
+    { img: 'img/owl.png', color:'#9f5927', word: 'owl', sound: 'sounds/owl' },
+    
+];
--- js/demo.js
+++ js/demo.js
@@ -2,30 +2,6 @@
 buzz.defaults.formats = ['ogg', 'mp3']; 
 buzz.defaults.preload = 'metadata'; 
 
-// Informações sobre os animais do jogo
-var games = [ 
-    { img: 'img/koala.png', color:'#176580', word: 'koala', sound: '' }, 
-    { img: 'img/elephant1.png', color:'#a36513', word: 'elephant', sound: 'sounds/elephant' },
-    { img: 'img/monkey.png', color:'#ffc48b', word: 'monkey', sound: 'sounds/monkey' },
-    { img: 'img/bear.png', color:'#807148', word: 'bear', sound: 'sounds/bear' },
-    { img: 'img/horse.png', color:'#bc9e6c', word: 'horse', sound: 'sounds/horse' },
-    { img: 'img/bull.png', color:'#ff5f09', word: 'bull', sound: 'sounds/bull' },
-    { img: 'img/rabbit.png', color:'#c81f27', word: 'rabbit', sound: '' },
-    { img: 'img/tiger.png', color:'#b3eef4', word: 'tiger', sound: 'sounds/meow' },
-    { img: 'img/turtle.png', color:'#d5ea86', word: 'turtle', sound: '' },
-    { img: 'img/lion1.png', color:'#dd992d', word: 'lion', sound: 'sounds/lion' },
-    { img: 'img/cicada.png', color:'#008000', word: 'cicada', sound: 'sounds/cicada' },
-    { img: 'img/fox.png', color:'#ffa500', word: 'fox', sound: 'sounds/fox' },
-    { img: 'img/pork.png', color:'#ffb6c1', word: 'pork', sound: 'sounds/pork' },
-    { img: 'img/whale.png', color:'#add8e6', word: 'whale', sound: 'sounds/whale' },
-    { img: 'img/snake.png', color:'#006400', word: 'snake', sound: 'sounds/snake' },
-    { img: 'img/fish.png', color:'#3232CD', word: 'fish', sound: 'sounds/fish' },
-    { img: 'img/frog.png', color:'#90ee90', word: 'frog', sound: 'sounds/frog' },
-    { img: 'img/dolphin.png', color:'#00ffff', word: 'dolphin', sound: 'sounds/dolphin' },
-    { img: 'img/shark.png', color:'#87cefa', word: 'shark', sound: 'sounds/shark' },
-    { img: 'img/owl.png', color:'#9f5927', word: 'owl', sound: 'sounds/owl' },
-    
-];
 
 // Função para criar instâncias de som
 function createSound(src) {
@@ -283,7 +259,7 @@ $(function() {
 
            // seleciona todos os elementos li e executa uma função para cada elemento
         $( '#letters li' ).each( function( i ) 
-        {   // cria uma variavel local que armazena uma referencia ao elemento 'li' atual
+        {   // cria uma variavel que armazena uma referencia ao elemento 'li' atual
             var $$ = $( this ); 
             setTimeout( function() {
                 $$.animate({
@@ -301,6 +277,13 @@ $(function() {
             buildGame( ++idx ); 
         }, 3000);
     }
+
+// Vinculando o botão de reinício ao evento de clique
+$('#reset').click(function() {
+    refreshGame(); // Limpa as alterações da página
+    buildGame(idx); // Reconstrói o jogo atual
+    return false; // Impede a ação padrão do link
+});
 
     //! o html a ser rotacionado('el') e o angulo da rotação('angle)
     function rotate( el, angle ) { 
